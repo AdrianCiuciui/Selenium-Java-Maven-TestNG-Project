ce929aa1f7309ea886abefe5132c391913758aa4
Ciuciui Adrian <adrian.ciuciui.cornel@gmail.com>
2023-11-18 00:44:25 +0200

    updated testng version

--- src/test/java/saucedemo/base/BaseTest.java
+++ src/test/java/saucedemo/base/BaseTest.java
@@ -3,6 +3,7 @@ package saucedemo.base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -38,7 +39,11 @@ public class BaseTest{
     public void setup() {
 
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.navigate().to(PROPERTIES.getProperty("url.base"));
         BasePageObject.setDriver(driver);
